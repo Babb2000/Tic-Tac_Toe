0b70c4b2a8665606ab3142870d1e2ec329251a94
Babb Getachew <babbgetachew@gmail.com>
2022-06-16 13:09:56 -0400

    Added button start game

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 $(document).ready(function(){
 
-   
+      
+
     $("#slideHeader").fadeIn(2000, ()=>{
         alert("Move your mouse under the question");
     });
+
+    function askAgain(){
+        window.location.reload(); //Keep reloading page each time user clicks no
+    }
     
+    function startGame(){
+        $("#slideHeader").slideUp();
+        $("#no").remove();
+        $("#yes").remove();
+    }
+
+
+    document.getElementById("no").onclick = function() {askAgain()};
+    document.getElementById("yes").onclick = function() {startGame()};
+
+    
+
+
+
 
 
 
