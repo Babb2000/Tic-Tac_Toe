f37f9f05741923248e2127bd23ae2db740a29acb
Babb Getachew <babbgetachew@gmail.com>
2022-06-18 15:19:19 -0400

    plan on rewriting functions with for-loops

--- script.js
+++ script.js
@@ -16,6 +16,53 @@ $(document).ready(function(){
           document.body.appendChild(holder1);
       }
 
+      function makeBoard(){
+        let firstDiv = document.createElement('div');
+        firstDiv.setAttribute("id", "gameBoard");
+        let secDiv = document.createElement('div');
+        secDiv.setAttribute("id", "realGameBoard");
+        let table = document.createElement('table');
+
+        let tr1 = document.createElement('tr');
+        let tr2 = document.createElement('tr');
+        let tr3 = document.createElement('tr');
+
+        let td1 = document.createElement('td');
+        let td2 = document.createElement('td');
+        let td3 = document.createElement('td');
+
+        let td4 = document.createElement('td');
+        let td5 = document.createElement('td');
+        let td6 = document.createElement('td');
+
+        let td7 = document.createElement('td');
+        let td8 = document.createElement('td');
+        let td9 = document.createElement('td');
+
+    
+        tr1.appendChild(td1);
+        tr1.appendChild(td2);
+        tr1.appendChild(td3);
+
+        tr2.appendChild(td4);
+        tr2.appendChild(td5);
+        tr2.appendChild(td6);
+
+        tr3.appendChild(td7);
+        tr3.appendChild(td8);
+        tr3.appendChild(td9);
+
+        table.appendChild(tr1);
+        table.appendChild(tr2);
+        table.appendChild(tr3);
+
+        secDiv.appendChild(table);
+        firstDiv.appendChild(secDiv);
+        document.body.appendChild(firstDiv);
+
+
+      }
+
 
     $("#slideHeader").fadeIn(2000, ()=>{
         chatBox();
@@ -25,16 +72,18 @@ $(document).ready(function(){
         window.location.reload(); //Keep reloading page each time user clicks no
     }
     
-    function startGame(){
+    function clearScreen(){
         $("#slideHeader").slideUp();
         $("#no").remove();
         $("#yes").remove();
         $(".buttonFlex2").remove();
+
+        makeBoard();
     }
 
 
     document.getElementById("no").onclick = function() {askAgain()};
-    document.getElementById("yes").onclick = function() {startGame()};
+    document.getElementById("yes").onclick = function() {clearScreen()};
 
     
 
