333c714ac1287b7b8c8803900565913ca6ef8648
Babb Getachew <babbgetachew@gmail.com>
2023-08-19 19:50:02 -0400

    Finished function to get user choice

--- script.js
+++ script.js
@@ -1,55 +1,29 @@
 
 
 //Event listener to gather user selection
-const button = document.querySelector('button');
-button.addEventListener("click", ()=>{
-  if(button.className === "X"){
-    alert("You have selected the weapon X");
-  }
-  if(button.className === "O"){
-    alert("You have selected the weapon O");
-  }
+const userSelection = ()=> {
+  
+  const buttonX = document.querySelector('.X');
+buttonX.addEventListener("click", ()=>{
+  alert("You chose to be X!!!");
 })
-
-
-
-
-const placeMarker = () =>{
-
-let grabber = document.querySelector("div").addEventListener("click", clickElement);
-
-function clickElement(e){
- 
-  let current = e.target;
-    
-  let holder = document.createElement('div');
-    
-    let p = document.createElement('p');
-    let markerX = 
-        document.createTextNode("X");
-     
-   p.appendChild(markerX);
-   p.style.fontSize = "100px";
-   p.style.textShadow = "1px 1px 4px white";
-   p.style.textAlign = "center";
-     
-   holder.classList.add("flex");
-   holder.appendChild(p);
-   
-   current.appendChild(holder);
-  }
-  
-  
-  
-
-  
-
-
+  const buttonO = document.querySelector(".O");
+  buttonO.addEventListener("click", ()=>{
+    alert("You chose to be O!!!");
+  })
 
 }
 
-const createBoard = () =>{
 
-}
+userSelection();
+
+
+
+
+
+
+
+
+
 
 
