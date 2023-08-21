379b0da50375a3489af6d78603008397fa23be31
Babb Getachew <babbgetachew@gmail.com>
2023-08-20 20:08:42 -0400

    creating intro controller

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 
 
-//Event listener to gather user selection
-const userSelection = ()=> {
-  
-  const buttonX = document.querySelector('.X');
-buttonX.addEventListener("click", ()=>{
-  alert("You chose to be X!!!");
-})
-  const buttonO = document.querySelector(".O");
-  buttonO.addEventListener("click", ()=>{
-    alert("You chose to be O!!!");
+function IntroController(){
+  const userSelection = ()=> {
+    const choiceX = "X";
+    const choiceO = "O";
+    const buttonX = document.querySelector('.X');
+  buttonX.addEventListener("click", ()=>{
+    return choiceX;
   })
+    const buttonO = document.querySelector(".O");
+    buttonO.addEventListener("click", ()=>{
+      return choiceO;
+    })
+  }
+  
+  let catchChoice = userSelection();
+  
+  
+  
+  //Factory function to assign player name and token
+  function playerAssign(choice){
+  
+  const player = {
+    choice: choice,
+  }
 
+  console.log(player);
+  
+  return player;
+  }
+  
+  playerAssign(catchChoice);
 }
 
-
-userSelection();
-
-
-
-
-
-
-
-
-
-
-
+IntroController();
