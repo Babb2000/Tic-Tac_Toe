2db3ce886773ee150d53c07284811a93283b2a2b
Babb Getachew <babbgetachew@gmail.com>
2023-08-21 19:46:48 -0400

    creating makingBoard function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 
 function IntroController(){
+
+
   const userSelection = ()=> {
     const choiceX = "X";
     const choiceO = "O";
@@ -44,3 +46,45 @@ userSelection().then((choice) =>{
 }
 
 IntroController();
+
+function makeBoard(){
+  for(let i = 0; i < 10; i++){
+    const div = document.createElement("div");
+    div.classList.add('edit');
+  }
+
+  
+
+  let grabber = document.querySelector("div").addEventListener("click", clickElement);
+
+function clickElement(e){
+ 
+  let current = e.target;
+    
+
+  let holder = document.createElement('div');
+    
+  let p = document.createElement('p');
+   let markerX = 
+       document.createTextNode("X");
+    
+  p.appendChild(markerX);
+  p.style.fontSize = "100px";
+  p.style.textShadow = "1px 1px 4px white";
+  p.style.textAlign = "center";
+    
+  holder.classList.add("flex");
+  holder.appendChild(p);
+  
+  current.appendChild(holder);
+  
+  
+}
+}
+
+
+
+
+
+
+
